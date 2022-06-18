36941d02ce2ad51cd21c8c2af4a678161a2b4491
AdmirDemaj <adidemaj@yahoo.com>
2022-06-18 19:16:46 +0200

    Changes

--- __int__test__/todos.test.js
+++ __int__test__/todos.test.js
@@ -3,21 +3,82 @@ const setupTestEnv = require('./setupTestEnv')
 
 const app = setupTestEnv();
 
-describe("Integretion tests for CRUD operations connected to test postgres ", async () =>{
+describe("Integretion tests for CRUD operations connected to test postgres ", () => {
+
     test("Should create an todo via POST route", async() => {
         const todo = {
             name: 'Test todo 2',
             description: 'This is a test todo',
-            gross_amount: 20
+            //gross_amount: 20,
             
-        }
+        };
         const response = await app.inject({
             method: "POST",
-            url: "/v2/",
-            payload: todo
-        })
+            url: "/v2",
+            payload: todo,
+        });
 
         expext(response.statusCode).toBe(201)
         expect(response.json()).toMatchObject(todo)
-    })
-})
+    });
+
+
+    test('Should get all ToDos', async () => {
+        const response = await app.inject({
+            method: 'GET',
+            url : '/v2',
+        });
+
+        expect(response.statusCode).toBe(200);
+        expect(response.json()).toMatchObject([{
+            description:'This is an test',
+            name: 'Test ToDo',
+        },
+    ]);
+    });
+
+
+    test('Should get a single ToDo by Id', async () => {
+        const response = await app.inject({
+            method : 'GET',
+            url: '/v2/1',
+        });
+
+        expect(response.statusCode).toBe(200);
+        expect(response.json()).toMatchObject([{
+            description: 'This is an test',
+            name: 'Test ToDo',
+           },
+         ]);
+        });
+
+        test('Should update a ToDo based on Id', async() => {
+            const todo = {
+                name: 'Updated ToDo',
+                description: 'This ToDo is updated',
+            };
+
+            const response = await app.inject({
+                method: 'PUT',
+                url: '/v2/2',
+                payload: todo
+            });
+            expect(response.statusCode).toBe(200);
+            expect(response.json()).toEqual(objectContaining(todo));
+
+        });
+
+        test('Should delete an ToDo based on Id', async () => {
+            const response = await app.inject({
+                method: 'DELETE',
+                url: '/v2/3'
+            });
+
+            expect(response.statusCode).toBe(200);
+            expect(response).toMatchObject({
+                body: 'Item with id 3 has been deleted',
+            });
+});
+
+    });
+
--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const {build} = require('./app')
 
-const env = request('./config/env')
+const env = require('./config/env')
 
 
 const app = build({logger:true}, {exposeRoute: true, routePrefix: '/docs', swagger:{info: {title:"Fastify-API", version: "1.0.0"}},},
